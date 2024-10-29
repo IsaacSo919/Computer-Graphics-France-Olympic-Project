bd03befee6ab902a02ce4f8d11b019fa49a2b0f3
IsaacSo919 <lmc919isaac@gmail.com>
2024-10-29 15:20:22 +0000

    camera is adjust to the right place

--- camera.py
+++ camera.py
@@ -12,7 +12,7 @@ class Camera:
         self.phi = 0.               # azimuth angle
         self.psi = 0.               # zenith angle
         self.distance = 50.0         # distance of the camera to the centre point
-        self.center = [0., 0., 0.]  # position of the centre
+        self.center = [0., -25., 0.]  # position of the centre
         self.update()               # calculate the view matrix
 
     def update(self):
